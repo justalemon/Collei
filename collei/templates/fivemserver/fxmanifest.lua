bd5857252898df4b17cc2c9d764cfcbf8517a9aa
Hannele Ruiz <justlemoncl@gmail.com>
2023-02-07 09:12:13 -0300

    Added template for FiveM Server DLL

---
+++ collei/templates/fivemserver/fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version "cerulean"
+games { "gta5" }
+
+{% if input.client %}client_script "{{ input.name }}.Client.net.dll"{% endif %}
+server_script "{{ input.name }}.Server.net.dll"
+{% if input.json %}file "Newtonsoft.Json.dll"{% endif %}
