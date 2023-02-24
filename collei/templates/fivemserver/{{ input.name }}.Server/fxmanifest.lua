d6befa449ca9b759d594c169b0a5f6c98caab622
Hannele Ruiz <justlemoncl@gmail.com>
2023-02-24 01:16:49 -0300

    Fixed FiveM Server template not working

--- collei/templates/fivemserver/{{ input.name }}.Server/fxmanifest.lua
+++ collei/templates/fivemserver/{{ input.name }}.Server/fxmanifest.lua
@@ -4,3 +4,4 @@ games { "gta5" }
 {% if input.client %}client_script "{{ input.name }}.Client.net.dll"{% endif %}
 server_script "{{ input.name }}.Server.net.dll"
 {% if input.json %}file "Newtonsoft.Json.dll"{% endif %}
+{% if input.lemonui %}file "LemonUI.FiveM.dll"{% endif %}
