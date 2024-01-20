9adc6baa9fe88281fc9310b030fb4b819d6cfc14
Hannele Ruiz <justlemoncl@gmail.com>
2024-01-20 00:30:16 -0300

    Make the SHVDN3 template include the Script and Configuration

---
+++ collei/templates/shvdn3/{{ input.name }}/Script.cs
@@ -0,0 +1,39 @@
+﻿using GTA;
+using System;
+using System.Windows.Forms;
+
+namespace {{ input.name }};
+
+public class {{ input.name }} : Script
+{
+    #region Fields
+    
+    private static readonly Configuration config = Configuration.Load();
+    
+    #endregion
+    
+    #region Constructors
+
+    public {{ input.name }}()
+    {
+        Tick += OnTick;
+        KeyDown += OnKeyDown;
+        KeyUp += OnKeyUp;
+    }
+    
+    #endregion
+    
+    #region Event Functions
+
+    private void OnTick(object sender, EventArgs e)
+    {
+    }
+    private void OnKeyDown(object sender, KeyEventArgs e)
+    {
+    }
+    private void OnKeyUp(object sender, KeyEventArgs e)
+    {
+    }
+
+    #endregion
+}
